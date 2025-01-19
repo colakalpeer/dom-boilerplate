6c3971336f9d23a68d5c1796a7aabe26aead2522
Alper Colak <colakalpeer93@gmail.com>
2025-01-20 02:15:36 +0300

    feat : description of the second lesson added.

--- lesson-2 getElementByname/app.js
+++ lesson-2 getElementByname/app.js
@@ -1,3 +1,6 @@
 let btn = document.getElementsByName("lang");
 
 console.log(btn);
+
+/* We assign names to our elements and then 
+call them according to the content of their names using getElementByName. */
