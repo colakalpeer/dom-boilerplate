b1d9431109957cc233eeeab9474dba5bfba12453
Alper Colak <colakalpeer93@gmail.com>
2025-01-20 02:12:14 +0300

    feat: added description line for what is done in the lesson

--- lesson-1 getElementById/app.js
+++ lesson-1 getElementById/app.js
@@ -1,3 +1,8 @@
 let dom = document.getElementById("DOM");
 
 console.log(dom);
+
+/**We have defined an id called “DOM” for our paragraph in our html element.
+Then we got this id as output in our console using getElementById.
+If there is no item with this ID on the web page, our console will return “null”.
+If there is more than one item with the same ID, only the first item will be returned.*/
