158cca4706f1d456e612f1ce0f163f0b29643c1d
Alper Colak <colakalpeer93@gmail.com>
2025-01-21 23:43:22 +0300

    feat: subject description added

--- lesson-4 getElementByClassName/app.js
+++ lesson-4 getElementByClassName/app.js
@@ -2,3 +2,6 @@ let cont = document.getElementById("container");
 let msg = cont.getElementsByClassName("message");
 
 console.log(msg);
+
+/* we can call the id we opened with getElementById and then we can access the classes
+in it with getElementsByClassName using the value (cont) we have given. */
