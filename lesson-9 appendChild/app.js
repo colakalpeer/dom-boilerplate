f06175575fe2db8b4a79daae53607b4f9b090d55
Alper Colak <colakalpeer93@gmail.com>
2025-01-28 18:39:20 +0300

    feat: I summarized the issue in the comment line

--- lesson-9 appendChild/app.js
+++ lesson-9 appendChild/app.js
@@ -4,3 +4,6 @@ let list = document.createElement("li");
 list.innerHTML = "Contact";
 
 menu.appendChild(list);
+
+// I added a fifth “li” element to the 4 “li” elements I gave in HTML by adding
+// list.innerHTML = “Contact”; via Js.
