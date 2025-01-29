5b07aa10be5feb3824aa2b13fcb58e93a4f59c58
Alper Colak <colakalpeer93@gmail.com>
2025-01-29 19:12:51 +0300

    feat: I have added a small explanation of the issue

--- lesson-11 innerHTML/app.js
+++ lesson-11 innerHTML/app.js
@@ -1,3 +1,6 @@
 let menu = document.getElementById("menu");
 
 menu.innerHTML = "<h1>Hello!</h1>";
+
+// menu.textContent = "<h1>Hello!</h1>";
+// If I use it the way I used it above, I get the text output “<h1>Hello!</h1>” directly
